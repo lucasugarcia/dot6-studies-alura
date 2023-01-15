cc30f74cbf3c2e6b1b14971a1396ffce80235543
Lucas Garcia <lucasugarcia@gmail.com>
2023-01-15 19:01:35 -0300

    Rota para retornar um filme por Id implementada

--- FilmesAPI/Controllers/FilmesController.cs
+++ FilmesAPI/Controllers/FilmesController.cs
@@ -8,10 +8,13 @@ namespace FilmesAPI.Controllers;
 public class FilmesController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
     public void AdicionaFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
+
         filmes.Add(filme);
 
         Console.WriteLine(filme.Titulo);
@@ -22,5 +25,11 @@ public class FilmesController : ControllerBase
     public IEnumerable<Filme> RecuperaFilmes()
     {
         return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperaFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(f => f.Id == id);
     }
 }
