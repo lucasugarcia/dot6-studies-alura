68e222432827cc2f52393db465f4baba997a7d1b
Lucas Garcia <lucasugarcia@gmail.com>
2023-01-15 22:38:44 -0300

    Rota para atualizar um filme parcialmente (PATCH)

--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddDbContext<FilmeContext>(opts => opts.UseMySql(connetionSting
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
