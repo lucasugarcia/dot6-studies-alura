ca96d70eeeeee30b01dcff879b9d1476ed026e83
Lucas Garcia <lucasugarcia@gmail.com>
2023-01-15 22:48:14 -0300

    Implementando rota para excluir um filme e atualizando o retorno de leitura também para um DTO

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -33,9 +33,9 @@ public class FilmeController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
-        return _context.Filmes.Skip(skip).Take(take);
+        return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
     }
 
     [HttpGet("{id}")]
@@ -46,7 +46,9 @@ public class FilmeController : ControllerBase
         if (filme == null)
             return NotFound();
 
-        return Ok(filme);
+        var filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+
+        return Ok(filmeDto);
     }
 
     [HttpPut("{id}")]
@@ -81,6 +83,20 @@ public class FilmeController : ControllerBase
 
         _mapper.Map(filmeParaAtualizar, filme);
 
+        _context.SaveChanges();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeletaFilme(int id)
+    {
+        var filme = _context.Filmes.FirstOrDefault(f => f.Id == id);
+
+        if (filme == null)
+            return NotFound();
+
+        _context.Remove(filme);
         _context.SaveChanges();
 
         return NoContent();
